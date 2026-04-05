4789c3afe4c7d54229857c6349cd9188d56ea26c
babacar22138 <babacarsow2001@gmail.com>
2026-04-05 22:53:45 +0200

    Sauvegarde des tâches.

--- asserts/js/main.js
+++ asserts/js/main.js
@@ -42,35 +42,83 @@ const interval = setInterval(mettreAJourTimer, 1000);
 
 // -------- ajout des tasks ---------
 
+// const inpTask = document.getElementById('add-task');
+// const selectPriority = document.getElementById('priority-select');
+// const btnAddTask = document.getElementById('btn-add');
+// const listTasks = document.querySelector('.list-tasks');
+// console.log(inpTask, selectPriority, btnAddTask, listTasks);
+// let taches = [];
+
+
+// btnAddTask.addEventListener('click', () =>{
+//     if(inpTask.value === ''){
+//         alert('Enter a task on the input asking.');
+//         return;
+//     }
+//     const taskTitle = inpTask.value;
+//     const taskPriority =selectPriority.value;
+
+//     const li = document.createElement('li');
+
+//     li.innerHTML = `
+//         <span class="colorTask ${taskPriority}"></span>
+//         <p>${taskTitle}</p>
+//         <input type="checkbox" name="validationTask" class="validationTask">
+//         <i class="fa-solid fa-trash"></i>
+//     `;
+
+//     li.setAttribute('draggable', true);
+//     listTasks.appendChild(li);
+//     taches.push({ titre: taskTitle, priorite: taskPriority });
+//     localStorage.setItem('taches', JSON.stringify(taches));
+
+//     inpTask.value = '';
+//     console.log(taskTitle, taskPriority);
+// });
+
+// console.log(localStorage.getItem('taches'))
+
+
+
+// ---------------- ajout de tache avec sauvegarde grâce à l'API js JSON
+
 const inpTask = document.getElementById('add-task');
 const selectPriority = document.getElementById('priority-select');
 const btnAddTask = document.getElementById('btn-add');
 const listTasks = document.querySelector('.list-tasks');
-console.log(inpTask, selectPriority, btnAddTask, listTasks);
+let taches = [];
 
+function creerTache(titre, priorite) {
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <span class="colorTask ${priorite}"></span>
+        <p>${titre}</p>
+        <input type="checkbox" name="validationTask" class="validationTask">
+        <i class="fa-solid fa-trash"></i>
+    `;
+    li.setAttribute('draggable', true);
+    listTasks.appendChild(li);
+}
 
-btnAddTask.addEventListener('click', () =>{
+const tachesSauvegardees = JSON.parse(localStorage.getItem('taches'));
+if(tachesSauvegardees) {
+    tachesSauvegardees.forEach(tache => creerTache(tache.titre, tache.priorite));
+    taches = tachesSauvegardees;
+}
+
+btnAddTask.addEventListener('click', () => {
     if(inpTask.value === ''){
         alert('Enter a task on the input asking.');
         return;
     }
     const taskTitle = inpTask.value;
-    const taskPriority =selectPriority.value;
+    const taskPriority = selectPriority.value;
 
-    const li = document.createElement('li');
-
-    li.innerHTML = `
-        <span class="colorTask ${taskPriority}"></span>
-        <p>${taskTitle}</p>
-        <input type="checkbox" name="validationTask" class="validationTask">
-        <i class="fa-solid fa-trash"></i>
-    `;
-
-    li.setAttribute('draggable', true);
-    listTasks.appendChild(li);
+    creerTache(taskTitle, taskPriority);
+    taches.push({ titre: taskTitle, priorite: taskPriority });
+    localStorage.setItem('taches', JSON.stringify(taches));
 
     inpTask.value = '';
-    console.log(taskTitle, taskPriority);
 });
 
 // -------- suppression des tasks ---------
